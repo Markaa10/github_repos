025bc306e9ef4e128e0e3877b5045d3a6cd0f33a
Markaa10 <rahulrajdahal@gmail.com>
2021-09-10 17:00:47 +0545

    api

---
+++ src/constants/api.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { get } from "lodash";
+
+axios.defaults.baseURL = "https://api.github.com/";
+
+export class Api {
+  async getUserRepos(payload: any) {
+    try {
+      const res = await axios.get(`/users/${payload}/repos`);
+
+      console.log("Get User repos response: ", res);
+      const data = get(res, "data");
+      return data;
+    } catch (e) {
+      console.log("Get user repos Error: ", e);
+      throw e;
+    }
+  }
+}
